4e39f1219835bb478eee4ed99a7ebc90d3edf299
“Mohab <mohab@el-abda3.net>
2024-01-02 18:26:35 +0200

    submit connected app screens

--- app/src/main/java/mrandroid/medicinereminder/activity/HomeActivity.java
+++ app/src/main/java/mrandroid/medicinereminder/activity/HomeActivity.java
@@ -1,14 +1,22 @@
 package mrandroid.medicinereminder.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import mrandroid.medicinereminder.R;
+import mrandroid.medicinereminder.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.fabAdd.setOnClickListener(view -> {
+            startActivity(new Intent(getBaseContext(), AddMedicineActivity.class));
+        });
     }
 }
