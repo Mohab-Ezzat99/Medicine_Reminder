4ac6e848b72ac5142d3a16149ac6c7649fbb6bec
“Mohab <mohab@el-abda3.net>
2024-01-03 18:40:37 +0200

    submit alarm manager

--- app/src/main/java/mrandroid/medicinereminder/activity/AddMedicineActivity.java
+++ app/src/main/java/mrandroid/medicinereminder/activity/AddMedicineActivity.java
@@ -1,46 +1,82 @@
 package mrandroid.medicinereminder.activity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
+import java.text.DateFormat;
+import java.util.Calendar;
+
 import mrandroid.medicinereminder.R;
+import mrandroid.medicinereminder.databinding.ActivityAddMedicineBinding;
+import mrandroid.medicinereminder.databinding.ActivityHomeBinding;
+import mrandroid.medicinereminder.util.AlarmReceiver;
 import mrandroid.medicinereminder.util.NotificationHelper;
 import mrandroid.medicinereminder.util.TimePickerFragment;
 
 public class AddMedicineActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
 
     private NotificationHelper notificationHelper;
-    private Button btn_notification;
-    private Button btn_time;
-    private TextView tvTimeValue;
+    private ActivityAddMedicineBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_medicine);
+        binding = ActivityAddMedicineBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         notificationHelper = new NotificationHelper(this);
         notificationHelper.checkNotificationPermission(this);
 
-        btn_notification = findViewById(R.id.btn_notification);
-        btn_time = findViewById(R.id.btn_time);
-        tvTimeValue = findViewById(R.id.tvTimeValue);
-
-        btn_notification.setOnClickListener(view -> notificationHelper.displayNotification("My title", "test body"));
-        btn_time.setOnClickListener(view -> {
+        binding.btnTime.setOnClickListener(view -> {
             DialogFragment timePicker = new TimePickerFragment();
             timePicker.show(getSupportFragmentManager(), "timePicker");
+        });
+
+        binding.btnCancelAlarm.setOnClickListener(view -> {
+            cancelAlarm();
         });
     }
 
     @Override
     public void onTimeSet(TimePicker timePicker, int hour, int minute) {
-        tvTimeValue.setText(hour + ":" + minute);
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, minute);
+        calendar.set(Calendar.SECOND, 0);
+
+        binding.tvTimeValue.setText(DateFormat.getTimeInstance(DateFormat.SHORT).format(calendar.getTime()));
+        startAlarm(calendar);
+    }
+
+    private void startAlarm(Calendar calendar) {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        if (calendar.before(Calendar.getInstance())) {
+            calendar.add(Calendar.DATE, 1); // if less then next day
+        }
+
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+    }
+
+    private void cancelAlarm() {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        alarmManager.cancel(pendingIntent);
+        Toast.makeText(this, "Alarm cancelled", Toast.LENGTH_SHORT).show();
     }
 }
